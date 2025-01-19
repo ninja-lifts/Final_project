e380cbbb6da278b1ee89755e1b7a42e5f330cc83
Nikhil jangir <nikhil23@iitk.ac.in>
2025-01-19 17:36:13 +0530

    Update training_model_RFC.py

--- training_model_RFC.py
+++ training_model_RFC.py
@@ -1,11 +1,7 @@
 import pickle
-# Import RandomForestClassifier from sklearn for creating a machine learning model
 from sklearn.ensemble import RandomForestClassifier
-# Import train_test_split to split the dataset into training and testing sets
 from sklearn.model_selection import train_test_split
-# Import accuracy_score to evaluate the model's performance
 from sklearn.metrics import accuracy_score
-# Import numpy for numerical operations and handling arrays
 import numpy as np
 
 # Load the dataset from the 'data.pickle' file using pickle
@@ -23,13 +19,13 @@ labels = np.asarray(data_dict['labels'])  # Corresponding class labels
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
 # Initialize the RandomForestClassifier, a robust machine learning model
-model = RandomForestClassifier()
+model_1 = RandomForestClassifier()
 
 # Train the model using the training data (features and labels)
-model.fit(x_train, y_train)
+model_1.fit(x_train, y_train)
 
 # Use the trained model to make predictions on the test data
-y_predict = model.predict(x_test)
+y_predict = model_1.predict(x_test)
 
 # Evaluate the model's performance by comparing predictions with the actual labels
 # `accuracy_score` calculates the percentage of correctly classified samples
@@ -40,6 +36,6 @@ print('{}% of samples were classified correctly !'.format(score * 100))
 
 # Save the trained model to a file named 'model.p' using pickle
 # The model is saved as a dictionary with the key 'model'
-f = open('model.p', 'wb')  # Open file in write-binary mode
-pickle.dump({'model': model}, f)  # Serialize and save the model
-f.close()  # Close the file to finalize saving
+f = open('model_1.p', 'wb')  # Open file in write-binary mode
+pickle.dump({'model': model_1}, f) 
+f.close()  
