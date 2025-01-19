5603a8bb7f3f6d3407bc5c93e9ad364a97390b57
Nikhil jangir <nikhil23@iitk.ac.in>
2025-01-19 17:17:26 +0530

    Update training_model_LR.py

--- training_model_LR.py
+++ training_model_LR.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 
-data_dict = pickle.load(open('./data.pickle', 'rb'))
+data_dict = pickle.load(open('./Downloads/data.pickle', 'rb'))
 
 data = np.asarray(data_dict['data'])
 labels = np.asarray(data_dict['labels'])
